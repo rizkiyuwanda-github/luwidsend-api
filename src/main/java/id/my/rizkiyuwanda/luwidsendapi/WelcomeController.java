f08a28008748c835f5b19d608b404a48f0e3faa5
Rizki Yuwanda <yuwanda.rizki@gmail.com>
2023-02-18 12:54:20 +0800

    init rest

---
+++ src/main/java/id/my/rizkiyuwanda/luwidsendapi/WelcomeController.java
@@ -0,0 +1,14 @@
+package id.my.rizkiyuwanda.luwidsendapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/latihan")
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "Welcome";
+    }
+}
