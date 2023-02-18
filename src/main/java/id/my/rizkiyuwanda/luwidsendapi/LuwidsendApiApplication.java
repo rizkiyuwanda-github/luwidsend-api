57b3b0db757baeff828d3f035b8990aeeb65052e
Rizki Yuwanda <yuwanda.rizki@gmail.com>
2023-02-18 11:30:25 +0800

    init awal

---
+++ src/main/java/id/my/rizkiyuwanda/luwidsendapi/LuwidsendApiApplication.java
@@ -0,0 +1,13 @@
+package id.my.rizkiyuwanda.luwidsendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuwidsendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuwidsendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/my/rizkiyuwanda/luwidsendapi/LuwidsendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.rizkiyuwanda.luwidsendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuwidsendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
